2e96a22abb5b856456c268591be311070f72ebba
mehjabinzamanmegha <mehjabinzamanmeghaaa@gmail.com>
2025-06-05 21:37:22 +0600

    Add files via upload

---
+++ Megha.py
@@ -0,0 +1,93 @@
+Write a python program that "Hello world"
+
+#print("Hello World")
+
+
+Write a python program to "add two numbers and print the result "
+
+#num1 =float(input("Enter the first number: "))
+#num2 =float(input("Enter the second number: "))
+#sum_result=num1+num2
+
+
+Write a python program that "takes your name as input and prints a greetings""
+
+#name="Assalamualaikum Megha!"
+#print(name + "How are you?")
+
+
+Write a python program that "Checks whether a number is even or odd"
+
+#num=int(input("Enter a number"))
+#if (num%2):
+#    print("Odd Number")
+#else:
+#    print("Even Number")
+
+
+Write a program that "finds the largest of two numbers")"
+
+#def Largest():
+#    number1=int(input("Enter the first number: "))
+#    number2=int(input("Enter the second number: "))
+#    if (number1>number2):
+#        print("The largest number is",number1)
+#    else:
+#        print("The largest number is",number2)
+#Largest()
+
+
+Write a simple calculaor that "adds,subtracts,multipiles and divies two numbers"
+
+#a= int(input("Enter the First number: "))
+#b= int(input("Enter the Second number: "))
+#sum=a+b
+#dif=a-b
+#mul=a*b
+#div=a/b
+#print("A+B =",sum)
+#print("A-B =",dif)
+#print("A*B =",mul)
+#print("A/B =",div)
+
+
+Write a program that prints numbers from 1 to 10 using a loop
+
+#=1
+#while i<=10:
+# print(i, end=",")
+# i =i+1
+
+
+Write a program to check whether a number is positive, negative or zero
+
+#a=int(input("Enter a number: "))
+#if a>0:
+#   print(a,"is a Positive Number")
+#    if a<0:
+#        print(a,"is a Negative Number")
+
+#b=int(input("Enter a number: "))
+#if b>0:
+#   print(b,"is a Negative Number")
+#   if b<0:
+#      print(b,"is a Positive Number")
+
+
+Write a program to swap the values of two veriables
+
+#a = input("Enter value for a: ")
+#b = input("Enter value for b: ")
+#print(f"\nBefore swapping:\na = {a}, b = {b}")
+#temp = a
+#a = b
+#b = temp
+#print(f"\nAfter swapping:\na = {a}, b = {b}")
+
+
+Write a program to convent tempareture from celsius to farenhit
+
+#celsius =float(input("Enter the Celsius Temperature = "))
+#farenheit = (celsius *1.8)+32
+#print("celsius to Farenheitn Temperature = ",farenheit)
+
